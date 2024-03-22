6d3f390760159fb888e5303131a1a3e480db894b
Aryan <aryansck@gmail.com>
2024-03-22 12:42:06 +0330

    librarianbooks - librarian-details add buttons

--- src/app/+pages/+librarians/librarians-books/librarians-books.component.ts
+++ src/app/+pages/+librarians/librarians-books/librarians-books.component.ts
@@ -3,18 +3,32 @@ import { BookService } from '../../../+services/book.service';
 import { Book } from '../../../+models/Book';
 import { MatTableModule } from'@angular/material/table';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-librarians-books',
   standalone: true,
   imports: [
     MatTableModule,
-    BookDetailsComponent
+    BookDetailsComponent,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './librarians-books.component.html',
   styleUrl: './librarians-books.component.css'
 })
 export class LibrariansBooksComponent implements OnInit{
+editData() {
+throw new Error('Method not implemented.');
+}
+removeData() {
+throw new Error('Method not implemented.');
+}
+addData() {
+  this.currentAction='add';
+}
+  currentAction='list';
   data:Book[]=[]
   ngOnInit(): void {
     this.data=this.bookService.getBooks();
