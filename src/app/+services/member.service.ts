bde4011eb0c57d4a7202d20fdbe99b1d23ca40ee
Aryan <aryansck@gmail.com>
2024-03-23 08:28:52 +0330

    librarian-members table created

--- src/app/+services/member.service.ts
+++ src/app/+services/member.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Member } from '../+models/Member';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  constructor() { }
+  private members:Member[]=[
+    {id:1001,isActive:true,fullname:'زهرا کریمی',gender:'خانم',phoneNumber:'09124356090',email:'zahrakarimi@gmail.com',address:'تهران'},
+    {id:1002,isActive:true,fullname:'سارا رمصانی',gender:'خانم',phoneNumber:'09184356090',email:'sararamezani@gmail.com',address:'همدان'},
+    {id:1003,isActive:true,fullname:'محسن ایمانی',gender:'آقا',phoneNumber:'09121230099',email:'Mohsenimani@gmail.com',address:'یزد'},
+    {id:1003,isActive:false,fullname:'مبین کریمی',gender:'آقا',phoneNumber:'09337896010',email:'mobinkarimi@gmail.com',address:'فارس'},
+  ];
+
+  getMembers(){
+    return this.members;
+  };
+
+  addMembers(member:Member){
+    return this.members.push(member);
+  };
 }
