395a986d17da7b2e3b899541801984f749a60a49
Aryan <aryansck@gmail.com>
2024-03-23 08:29:30 +0330

    librarian-members table html-css-ts  created

---
+++ src/app/+models/Member.ts
@@ -0,0 +1,10 @@
+export interface Member{
+  id:Number;
+  isActive:boolean;
+  fullname:string;
+  gender:string;
+  phoneNumber:string;
+  email:string;
+  address:string;
+
+}
--- src/app/+pages/+librarians/librarians-members/librarians-members.component.ts
+++ src/app/+pages/+librarians/librarians-members/librarians-members.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MemberService } from '../../../+services/member.service';
+import { Member } from '../../../+models/Member';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-librarians-members',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './librarians-members.component.html',
   styleUrl: './librarians-members.component.css'
 })
-export class LibrariansMembersComponent {
+export class LibrariansMembersComponent implements OnInit{
+  data:Member[]=[]
+  memberService=inject(MemberService);
 
+  ngOnInit(): void {
+    this.data=this.memberService.getMembers();
+  }
+  displayedColumns: string[] = [ 'isActive', 'fullname', 'gender', 'phoneNumber','email','address'];
 }
